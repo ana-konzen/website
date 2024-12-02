b9a6bb3aee5d39be1f2445d4e73f81a29a9ceb14
ana-konzen <143814540+ana-konzen@users.noreply.github.com>
2024-12-02 00:06:20 -0500

    update

--- script.js
+++ script.js
@@ -20,9 +20,6 @@ let GIFInterval = setInterval(getGIF, 5000);
 projects.forEach((project) => {
   const projectItem = document.createElement("div");
   projectItem.innerHTML = `<a href="./projects/${project.toLowerCase().split(" ").join("-")}">${project}</a>`;
-  if (project === "dungeon mystery") {
-    projectItem.innerHTML = `<a href="">${project}</a>`;
-  }
   projectItem.classList.add("project-item");
   projectItem.classList.add(project.toLowerCase().split(" ").join("-"));
   projectList.appendChild(projectItem);
